774c593443aa4973cd51b46bf4c05703292f6291
Jonas Jensen <jbj@knef.dk>
2021-07-05 10:26:37 +0200

    Basics: JSON and a Point type

--- Playground/Program.fs
+++ Playground/Program.fs
@@ -1,6 +1,13 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open System.Text.Json
+
+[<Struct>]
+type Point = {
+    X: int
+    Y: int
+}
 
 // Define a function to construct a message to print
 let from whom =
@@ -10,4 +17,9 @@ let from whom =
 let main argv =
     let message = from "F#" // Call the function
     printfn "Hello world %s" message
+    let p = { X = 11; Y = 22 }
+    let pJSON = JsonSerializer.Serialize p
+    printfn "pJSON = %s" pJSON
+    let p' : Point = JsonSerializer.Deserialize pJSON
+    printfn "p' =\n%A" p'
     0 // return an integer exit code
