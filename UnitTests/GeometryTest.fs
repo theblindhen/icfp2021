7c74d61be4acbfda8dd34d52c51677418365f602
Christoffer Nielsen <chroni@live.dk>
2021-07-09 22:08:00 +0200

    fixed error

--- UnitTests/GeometryTest.fs
+++ UnitTests/GeometryTest.fs
@@ -12,13 +12,13 @@ type GeometryTestClass () =
     member this.TestSortSegment1 () =
         let segment1 = (Coord (2, 0), Coord (4, 2));
         let expected1 = (Coord (2, 0), Coord (4, 2));
-        Assert.AreEqual(expected1, sortedSegment segment1)
+        Assert.AreEqual(expected1, sortSegment segment1)
 
     [<TestMethod>]
-   member this.TestSortSegment2 () =
+    member this.TestSortSegment2 () =
         let segment1 = (Coord (2, 2), Coord (3, 1));
         let expected1 = (Coord (3, 1), Coord (2, 2));
-        Assert.AreEqual(expected1, sortedSegment segment1)
+        Assert.AreEqual(expected1, sortSegment segment1)
 
     [<TestMethod>]
     member this.TestSortSegments () =
