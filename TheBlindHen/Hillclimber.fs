3dc0aac5ef4847e333ec8caea52545b1b5f068c5
Kasper Svendsen <kaspersv@gmail.com>
2021-07-09 18:13:38 +0200

    added simple hill-climber implementation

---
+++ TheBlindHen/Hillclimber.fs
@@ -0,0 +1,32 @@
+module Hillclimber
+
+let cons x y = x :: y
+
+
+// generates an infinite sequence of solutions using hill-climbing
+// to visit neighbors with the lowest cost (or the current solution,
+// if no neighbors are better)
+//
+// the neighbors function may be non-deterministic; if the neighbors function
+// is deterministic, then repeats in the solution sequence implies that a local
+// minimum has been reached
+let hillClimber neighbors cost =
+    Seq.unfold (fun solution -> 
+        solution 
+        |> neighbors 
+        |> cons solution
+        |> List.minBy cost 
+        |> (fun x -> Some (x, x)))
+
+
+// optimize the given solution using hill-climbing with the given
+// neighbor and cost function; stops after the given number of max
+// iterations or the first solution with a non-positive cost
+// maximum number of iterations must be positive
+let runHillClimber neighbors cost maxIterations solution =
+    Seq.append (Seq.singleton solution) (hillClimber neighbors cost solution)
+    |> Seq.pairwise
+    |> Seq.take maxIterations
+    |> Seq.takeWhile (fun (prevSol, _) -> cost prevSol > 0.0)
+    |> Seq.last
+    |> snd
--- UnitTests/Tests.fs
+++ UnitTests/Tests.fs
@@ -13,3 +13,12 @@ type TestClass () =
     [<TestMethod>]
     member this.TestFrom () =
         Assert.AreEqual("from monkey", Program.from "monkey")
+
+    [<TestMethod>(fun d -> d)(fun d -> d)]
+    member this.TestHillClimber () =
+        Assert.AreEqual(2.0, Hillclimber.runHillClimber (fun d -> [d - 0.5; d + 0.5]) id 1 2.5)
+        Assert.AreEqual(1.5, Hillclimber.runHillClimber (fun d -> [d - 0.5; d + 0.5]) id 2 2.5)
+        Assert.AreEqual(1.0, Hillclimber.runHillClimber (fun d -> [d - 0.5; d + 0.5]) id 3 2.5)
+        Assert.AreEqual(0.5, Hillclimber.runHillClimber (fun d -> [d - 0.5; d + 0.5]) id 4 2.5)
+        Assert.AreEqual(0.0, Hillclimber.runHillClimber (fun d -> [d - 0.5; d + 0.5]) id 5 2.5)
+        Assert.AreEqual(0.0, Hillclimber.runHillClimber (fun d -> [d - 0.5; d + 0.5]) id 10 2.5)
