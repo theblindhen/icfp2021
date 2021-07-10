aea35bc51306940ae77a182f906373b886ffa90f
Johan Rosenkilde <jsrn@jsrn.dk>
2021-07-10 17:40:40 +0200

    new segmentDecomposition

--- TheBlindHen/Geometry.fs
+++ TheBlindHen/Geometry.fs
@@ -17,6 +17,10 @@ type Vector =
             sqrt(this.X*this.X + this.Y*this.Y)
     end
 
+let EPSILON = 0.001
+let isZero (x: float) = abs (x) < EPSILON
+let isOne (x: float) = isZero (x - 1.0)
+
 /// Invariant that vertices V1-3 are sorted in increasing order of their Y-coordinate
 /// constructor will sort input
 // type Triangle = 
@@ -61,32 +65,42 @@ let sortSegments (segments : Segment list) =
 // let innerCoordsOfSimplePolygon  segments =
 //     let edges
 
-let EPSILON = 0.001
-
 let vectorOfSegment ((s1, s2): Segment) =
     Vector (float (s1.X - s2.X), float (s1.Y - s2.Y)) 
 
 let vectorDotProduct (v1: Vector) (v2: Vector) =
     v1.X * v2.X + v1.Y * v2.Y
+        
+type Direction = CW | CCW
+
+// TODO: These should all operate on Segments to be ints
 
 let vectorsAreParallel (v1: Vector) (v2: Vector) =
     let v2hat = Vector(-v2.Y, v2.X)
     abs (vectorDotProduct v1 v2hat) < EPSILON
+
+let vectorDeterminant (v1: Vector) (v2: Vector) : float =
+    v1.X * v2.Y - v1.Y * v2.X
+
+let _determinantToDirection (d: float) : Direction =
+    if d > 0.0 then CCW else CW
+
+let vectorsDirection (v1: Vector) (v2: Vector) : Direction =
+    _determinantToDirection (vectorDeterminant v1 v2)
 
 /// Solve a 2x2 matrix problem by inversion.
 /// Returns None if the matrix is singular.
 /// Note that there may still be a solution, if the column space is
 /// 1-dimensional and the target vector happens to fall within that.
 let solveByInversion(row1: Vector, row2: Vector) (target: Vector) =
-    let d = row1.X * row2.Y - row1.Y * row2.X
-    if abs(d) < EPSILON then
+    let d = vectorDeterminant row1 row2
+    if isZero d then
         None
     else
         let sX = target.X * row2.Y - target.Y * row2.X
         let sY = target.X * row1.Y - target.Y * row1.X
         Some (Vector (sX/d, sY/d), d)
-        
-type Direction = CW | CCW
+
 type SegmentIntersect = 
       /// Multiplier on seg1 to get the intersection point,
       /// Dir from seg1 to seg2
@@ -102,7 +116,7 @@ let segmentsIntersect (seg1 : Segment) (seg2: Segment) : SegmentIntersect option
     let p2,q2 = seg2
     let startDiff = Vector(float (p2.X - p1.X), float (p2.Y - p1.Y))
     match solveByInversion (v1, v2) startDiff with
-    | Some (sol, det) -> Some (Point (-sol.X, (if det < 0. then CW else CCW),  -sol.Y))
+    | Some (sol, det) -> Some (Point (-sol.X, _determinantToDirection det,  -sol.Y))
     | None ->
         // The segments are parallel.
         // They are then on the same line exactly when startDiff is parallel to v1
@@ -112,28 +126,32 @@ let segmentsIntersect (seg1 : Segment) (seg2: Segment) : SegmentIntersect option
         else
             None
 
-let segmentIntersectionList (seg: Segment) (intersectors: Segment list) : SegmentIntersect list =
+let segmentIntersectionList (seg: Segment) (intersectors: Segment list) : (int * SegmentIntersect) list =
     let ltOne a = a - EPSILON < 1.0
     let gtZero a = a + EPSILON > 0.0
     intersectors 
-    |> List.choose (segmentsIntersect seg)
+    |> List.indexed
+    |> List.choose (fun (i, ints) ->
+        match segmentsIntersect seg ints with
+        | None -> None
+        | Some ints -> Some (i, ints))
     |> List.map (function
-        | Overlap (a,b) ->
+        | i, Overlap (a,b) ->
             let a, b = 
                 if a < b then
                     max a 0., min b 1.
                 else
                     max b 0., min a 1.
-            Overlap (a, b)
-        | ints -> ints)
+            (i, Overlap (a, b))
+        | iints -> iints)
     |> List.filter (function
-        | Point (a,_,b) -> gtZero a && ltOne a && gtZero b && ltOne b
-        | Overlap (a,b) -> ltOne a && gtZero b)
+        | _, Point (a,_,b) -> gtZero a && ltOne a && gtZero b && ltOne b
+        | _, Overlap (a,b) -> ltOne a && gtZero b)
 
 type IncidenceType = Touch | Cross
 type Decomposition =
     | DecPoint of float * IncidenceType
-    | DecOverlap of float * float * IncidenceType
+    | DecOverlap of float * float
 
 /// Decompose a segment according to its intersection with a simple polygon
 /// This assumes that the simplePolygon's segments are given in order, i.e. that
@@ -146,6 +164,9 @@ let segmentDecomposition (seg: Segment) (simplePolygon: Segment list) : Decompos
             abs (a - b1) < EPSILON || abs (a - b2) < EPSILON
         | _ -> false
     let intersections = segmentIntersectionList seg simplePolygon
+    if List.isEmpty intersections then
+        []
+    else
     // // Pass over intersections and remove the Points that are adjacent to
     // // Overlaps
     // let intersections = 
@@ -177,31 +198,38 @@ let segmentDecomposition (seg: Segment) (simplePolygon: Segment list) : Decompos
     // Convert to Decomposition
     // Convert repeated points to CrossPoint / TouchPoint
     let decompositions =
-        let crossOrTouch a adir bdir =
-            if adir = bdir then
-                DecPoint(a, Touch)
-            else
-                DecPoint(a, Cross)
-        let (decompositions_rev, olastPnt) = 
-            List.fold (fun (acc, lastPoint) cur ->
-                match (lastPoint, cur) with
-                | Some (a, adir, hadAlign), Point (b,bdir,_) ->
-                    if not hadAlign && abs (a - b) < EPSILON then
-                        (crossOrTouch a adir bdir :: acc, None)
-                    else
-                        (DecPoint (a, Cross) :: acc, Some (b, bdir, false))
-                | Some (a, adir, _), Overlap (b1, b2) ->
-                    if abs (a - b1) < EPSILON || abs (a - b2) < EPSILON then 
-                        // TODO: Don't know yet whether cross or touch
-                        (DecOverlap (b1,b2, Cross) :: acc, Some (a, adir, true))
-                    else
-                        (DecOverlap (b1,b2, Cross) :: acc, Some (a, adir, true))
-                | None, Point (a, adir,_) ->
-                    (acc, Some (a, adir, false))
-                | None, Overlap (b1, b2) ->
-                    // Don't know yet whether touch or cross
-                    (DecOverlap (b1,b2, Cross) :: acc, None)
-                ) ([], None) intersections
+        let lastDir  =
+            let firstIdx, _ = intersections.[0]
+            let segArr = Array.ofList simplePolygon
+            let segv = vectorOfSegment seg
+            let rec getLastDir i =
+                let i = if i = 0 then segArr.Length-1 else i - 1
+                let holeSegv = vectorOfSegment segArr.[i]
+                let d = vectorDeterminant segv holeSegv
+                if d < EPSILON then
+                    getLastDir i
+                else
+                    vectorsDirection segv holeSegv
+            getLastDir firstIdx
+        let (decompositions_rev, lastDir) = 
+            List.fold (fun (acc, lastDir) cur ->
+                match cur with
+                | Point (acur, curDir, bcur) ->
+                    let typ =
+                        match isZero bcur, isOne bcur with
+                        | false, false ->
+                            // In the middle of a hole segment
+                            Cross
+                        | _, true ->
+                            // At the end of a hole segment
+                             Touch
+                        | true, _ -> 
+                            // At the beginning of a hole segment
+                            if curDir = lastDir then Cross else Touch
+                    (DecPoint (acur, typ)::acc, curDir)
+                | Overlap (a1, a2) ->
+                    (DecOverlap (a1, a2)::acc, lastDir)
+                ) ([], lastDir) (List.map snd intersections)
         let decompositions = List.rev decompositions_rev
         decompositions
         // Special case: Last and first is Point 
@@ -216,7 +244,7 @@ let segmentDecomposition (seg: Segment) (simplePolygon: Segment list) : Decompos
     decompositions
     |> List.sortBy (function
         | DecPoint (a,_) -> a
-        | DecOverlap (a,_,_) -> a)
+        | DecOverlap (a,_) -> a)
 
 
 /// A structure for precomputed set of points in the hole
