61029d4c4e31640591f02064cd58c0e9709b704d
Johan Rosenkilde <jsrn@jsrn.dk>
2021-07-11 00:13:47 +0200

    wip Debugging segmentOutsideHole

--- UnitTests/PenaltyTests.fs
+++ UnitTests/PenaltyTests.fs
@@ -33,6 +33,29 @@ type SegmentOutsideHoleTestClass () =
         seg (8,9) (4,15)
         seg (4,15) (-2,6)
     ]
+    // Inspired by Problem 4
+    let wedgeHole = 
+        [
+            seg (70,90) (50,95)
+            seg (50,95) (30,90)
+            seg (30,90) (30,0)
+            seg (30, 0) (70,0)
+            seg (70,0) (70,90)
+        ]
+    let sharpWedgeHole = 
+        [
+            seg (0, 20) (2, 10)
+            seg (2, 10) (4, 20)
+            seg (4, 20) (0, 20)
+        ]
+    let invSharpWedgeHole = 
+        [
+            seg (-30, -100) (0, 20)
+            seg (0, 20) (2, 10)
+            seg (2, 10) (4, 20)
+            seg (4, 20) (34, 100)
+            seg (34, -100) (0, -30)
+        ]
 
     [<TestMethod>]
     member this.TestIsCoordInsideHole1 () = 
@@ -73,6 +96,55 @@ type SegmentOutsideHoleTestClass () =
     [<TestMethod>]
     member this.TestIsCoordInsideHole10 () = 
         Assert.AreEqual(false, isCoordInsideHole testHole2 (Coord(3,3)))
+
+    [<TestMethod>]
+    member this.TestIsCoordInsideHole11 () = 
+        Assert.AreEqual(true, isCoordInsideHole wedgeHole (Coord(50,95)))
+
+
+
+    [<TestMethod>]
+    member this.TestSegmentStartsInside1 () = 
+        let segment = seg (2,2) (3,3) 
+        Assert.IsTrue(segmentStartsInside testHole1 segment)
+
+    [<TestMethod>]
+    member this.TestSegmentStartsInside2 () = 
+        let segment = seg (2,2) (4,4) 
+        Assert.IsTrue(segmentStartsInside testHole1 segment)
+
+    [<TestMethod>]
+    member this.TestSegmentStartsInside3 () = 
+        let segment = seg (0,0) (4,4) 
+        Assert.IsFalse(segmentStartsInside testHole1 segment)
+
+    [<TestMethod>]
+    member this.TestSegmentStartsInside4 () = 
+        let segment = seg (0,2) (8,2) 
+        Assert.IsFalse(segmentStartsInside testHole1 segment)
+
+    [<TestMethod>]
+    member this.TestSegmentStartsInside5 () = 
+        let segment = seg (2,0) (1,1) 
+        Assert.IsFalse(segmentStartsInside testHole2 segment)
+
+    [<TestMethod>]
+    member this.TestSegmentStartsInside6 () = 
+        let segment = seg (1,1) (2,0)
+        Assert.IsFalse(segmentStartsInside testHole2 segment)
+
+    [<TestMethod>]
+    member this.TestSegmentStartsInside7 () = 
+        let segment = seg (0,-3) (4,3)
+        Assert.IsFalse(segmentStartsInside testHole3 segment)
+
+    [<TestMethod>]
+    member this.TestSegmentStartsInside8 () = 
+        let segment = seg (0,-3) (6,6)
+        Assert.IsFalse(segmentStartsInside testHole3 segment)
+
+
+
 
     [<TestMethod>]
     member this.TestSegmentOutsideHole1 () = 
@@ -130,6 +202,41 @@ type SegmentOutsideHoleTestClass () =
         Assert.AreEqual(0.4, segmentOutsideHole testHole3 segment, EPSILON)
 
     [<TestMethod>]
+    member this.TestSegmentOutsideHole12 () = 
+        let seg = seg (40,85) (50,95)
+        Assert.AreEqual(0.0, segmentOutsideHole wedgeHole seg, EPSILON)
+
+    [<TestMethod>]
+    member this.TestSegmentOutsideHole13 () = 
+        let seg = seg (50,95) (60,85)
+        Assert.AreEqual(0.0, segmentOutsideHole wedgeHole seg, EPSILON)
+
+    [<TestMethod>]
+    member this.TestSegmentOutsideHole14 () = 
+        let seg = seg (50,95) (50,100)
+        Assert.AreEqual(1.0, segmentOutsideHole wedgeHole seg, EPSILON)
+
+    [<TestMethod>]
+    member this.TestSegmentOutsideHole15 () = 
+        let seg = seg (2, 10) (2, 0)
+        Assert.AreEqual(1.0, segmentOutsideHole sharpWedgeHole seg, EPSILON)
+
+    [<TestMethod>]
+    member this.TestSegmentOutsideHole16 () = 
+        let seg = seg (2, 10) (10, 10)
+        Assert.AreEqual(1.0, segmentOutsideHole sharpWedgeHole seg, EPSILON)
+
+    [<TestMethod>]
+    member this.TestSegmentOutsideHole17 () = 
+        let seg = seg (2, 10) (2, 0)
+        Assert.AreEqual(0.0, segmentOutsideHole invSharpWedgeHole seg, EPSILON)
+
+    [<TestMethod>]
+    member this.TestSegmentOutsideHole18 () = 
+        let seg = seg (2, 10) (10, 10)
+        Assert.AreEqual(0.0, segmentOutsideHole invSharpWedgeHole seg, EPSILON)
+
+    [<TestMethod>]
     member this.TestSortSegments () =
         let segments =
             [
@@ -146,3 +253,8 @@ type SegmentOutsideHoleTestClass () =
                (Coord (4, 2), Coord (2, 4));
             ]
         Assert.AreEqual(expectedSolutions, sortSegments segments)
+
+    [<TestMethod>]
+    member this.TestPenaltyEdgeRatioOutside () = 
+        let segment = seg (-2,2) (8,17)
+        Assert.AreEqual(0.4, segmentOutsideHole testHole3 segment, EPSILON)
