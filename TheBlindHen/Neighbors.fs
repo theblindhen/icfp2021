9d08157d365bf09f4f4653a9d0ab513bdc324ab3
Kasper Svendsen <kaspersv@gmail.com>
2021-07-10 20:20:07 +0200

    add rotation neighbor function

--- TheBlindHen/Neighbors.fs
+++ TheBlindHen/Neighbors.fs
@@ -17,6 +17,11 @@ let translateFullFigureRandomly (rnd: System.Random) (figure: Figure) =
     let delta = directions.[rnd.Next(directions.Length)]
     Transformations.translateVerticies delta figure
 
+let rotateFullFigureAroundRandomPoint (rnd: System.Random) (figure: Figure) =
+    let rndIndex = rnd.Next(figure.Vertices.Length)
+    let rndCord = Array.item rndIndex figure.Vertices
+    Transformations.rotateVerticiesAround rndCord figure
+ 
 let weightedChoice (choices: (float * (System.Random -> 'a -> 'b)) list) (rnd: System.Random) (param: 'a) : 'b =
     let totalWeight = List.sumBy fst choices
     let randomNumber = rnd.NextDouble() * totalWeight
@@ -41,4 +46,5 @@ let balancedCollectionOfNeighbors (rnd: System.Random) (figure: Figure) =
     weightedChoice [
         4.0, translateRandomCoord;
         1.0, translateFullFigureRandomly;
+//      0.1, rotateFullFigureAroundRandomPoint;
     ] rnd figure
