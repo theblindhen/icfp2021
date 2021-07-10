dfe124352df6c33f6396a991cc73df1d71b59e04
Christoffer Nielsen <chroni@live.dk>
2021-07-10 09:55:25 +0200

    Fixed and added decompose and intersectionLists unit tests

--- UnitTests/GeometryTest.fs
+++ UnitTests/GeometryTest.fs
@@ -6,7 +6,10 @@ open Geometry
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
 [<TestClass>]
-type GeometryTestClass () =
+type GeometrySortTestClass () =
+
+    // Helpers
+    let seg (x1,y1) (x2,y2) = (Coord (x1,y1), Coord (x2,y2))
 
     [<TestMethod>]
     member this.TestSortSegment1 () =
@@ -38,109 +41,233 @@ type GeometryTestClass () =
             ]
         Assert.AreEqual(expectedSolutions, sortSegments segments)
 
+[<TestClass>]
+type GeometryIntersectTestClass () =
+
+    // Helpers
+    let seg (x1,y1) (x2,y2) = (Coord (x1,y1), Coord (x2,y2))
+
     [<TestMethod>]
     member this.TestSegmentsIntersectParallel1 () =
         let seg1 = (Coord (0, 0), Coord (0, 2))
         let seg2 = (Coord (2, 0), Coord (2, 2))
-        let expected = Parallel
+        let expected = None
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)
  
     [<TestMethod>]
     member this.TestSegmentsIntersectParallel2 () =
         let seg1 = (Coord (4, 2), Coord (8, 4))
         let seg2 = (Coord (2, 0), Coord (6, 2))
-        let expected = Parallel
+        let expected = None
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)
 
     [<TestMethod>]
     member this.TestSegmentsIntersectParallel3 () =
         let seg1 = (Coord (8, 4), Coord (4, 2))
         let seg2 = (Coord (2, 0), Coord (6, 2))
-        let expected = Parallel
+        let expected = None
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)
 
     [<TestMethod>]
     member this.TestSegmentsIntersectOverlap1 () =
         let seg1 = (Coord (0, 0), Coord (4, 4))
         let seg2 = (Coord (1, 1), Coord (2, 2))
-        let expected = Overlap (0.25, 0.5)
+        let expected = Some (Overlap (0.25, 0.5))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)       
 
     [<TestMethod>]
     member this.TestSegmentsIntersectOverlap2 () =
         let seg1 = (Coord (2, 2), Coord (6, 4))
         let seg2 = (Coord (4, 3), Coord (8, 5))
-        let expected = Overlap (0.5, 1.5)
+        let expected = Some (Overlap (0.5, 1.5))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)       
 
     [<TestMethod>]
     member this.TestSegmentsIntersectOverlap3 () =
         let seg1 = (Coord (9, 6), Coord (1, 2))
         let seg2 = (Coord (3, 3), Coord (5, 4))
-        let expected = Overlap (0.75, 0.5)
+        let expected = Some (Overlap (0.75, 0.5))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)       
 
     [<TestMethod>]
     member this.TestSegmentsIntersectOverlap4 () =
         let seg1 = (Coord (6, 7), Coord (7, 8))
         let seg2 = (Coord (2, 3), Coord (1, 2))
-        let expected = Overlap (-4.0, -5.0)
+        let expected = Some (Overlap (-4.0, -5.0))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)       
 
     [<TestMethod>]
     member this.TestSegmentsIntersectPoint1 () =
         let seg1 = (Coord (0, 0), Coord (2, 2))
         let seg2 = (Coord (2, 0), Coord (0, 2))
-        let expected = Point (0.5, 0.5)
+        let expected = Some (Point (0.5))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)
 
     [<TestMethod>]
     member this.TestSegmentsIntersectPoint2 () =
         let seg1 = (Coord (0, 0), Coord (2, 2))
         let seg2 = (Coord (1, 0), Coord (0, 1))
-        let expected = Point (0.25, 0.5)
+        let expected = Some (Point (0.25))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)    
 
     [<TestMethod>]
     member this.TestSegmentsIntersectPoint3 () =
         let seg1 = (Coord (0, 2), Coord (3, 7))
         let seg2 = (Coord (2, 0), Coord (3, 7))
-        let expected = Point (1.0, 1.0)
+        let expected = Some (Point (1.0))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)      
 
     [<TestMethod>]
     member this.TestSegmentsIntersectPoint4 () =
         let seg1 = (Coord (3, 2), Coord (3, 7))
         let seg2 = (Coord (3, 7), Coord (-1, 2))
-        let expected = Point (1.0, 0.0)
+        let expected = Some (Point (1.0))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2) 
 
     [<TestMethod>]
     member this.TestSegmentsIntersectPoint5 () =
         let seg1 = (Coord (4, 4), Coord (0, 0))
         let seg2 = (Coord (1, 7), Coord (5, -1))
-        let expected = Point (0.25, 0.5)
+        let expected = Some (Point (0.25))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)      
 
     [<TestMethod>]
     member this.TestSegmentsIntersectPoint6 () =
         let seg1 = (Coord (0, 0), Coord (2, 2))
         let seg2 = (Coord (0, 2), Coord (2, 0))
-        let expected = Point (0.5, 0.5)
+        let expected = Some (Point (0.5))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)
 
     [<TestMethod>]
     member this.TestSegmentsIntersectPoint7 () =
         let seg1 = (Coord (0, 0), Coord (2, 2))
         let seg2 = (Coord (0, 2), Coord (-2, 4))
-        let expected = Point (0.5, -0.5)
+        let expected = Some (Point (0.5))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)
 
     [<TestMethod>]
     member this.TestSegmentsIntersectPoint8 () =
         let seg1 = (Coord (2, 2), Coord (3, 3))
         let seg2 = (Coord (-2, 2), Coord (-3, 3))
-        let expected = Point (-2.0, -2.0)
+        let expected = Some (Point (-2.0))
         Assert.AreEqual(expected, segmentsIntersect seg1 seg2)
 
+[<TestClass>]
+type GeometryIntersectionListTestClass () =
 
+    // Helpers
+    let seg (x1,y1) (x2,y2) = (Coord (x1,y1), Coord (x2,y2))
+
+    [<TestMethod>]
+    member this.TestSegmentItersectionListPoint1 () =
+        let s1 = seg (0,0) (1,1)
+        let segs =
+            [
+                seg (0,1) (1,0)
+                seg (0,2) (1,1)
+                seg (0,3) (1,2)
+                seg (0,4) (1,3)
+            ]
+        let expected =
+            [
+                Point (0.5)
+                Point (1.0)
+            ]
+        Assert.AreEqual(expected, segmentIntersectionList s1 segs)
+
+    [<TestMethod>]
+    member this.TestSegmentItersectionListPoint2 () =
+        let s1 = seg (0,0) (-1,-1)
+        let segs =
+            [
+                seg (1,0) (0,1)
+                seg (2,0) (1,1)
+                seg (3,0) (2,1)
+                seg (4,0) (3,1)
+            ]
+        let expected : SegmentIntersect list = []
+        Assert.AreEqual(expected, segmentIntersectionList s1 segs)
+
+    [<TestMethod>]
+    member this.TestSegmentItersectionListPoint3 () =
+        let s1 = seg (0,0) (1,1)
+        let segs =
+            [
+                seg (1,1) (2,2)
+                seg (1,2) (2,3)
+                seg (0,1) (1,2)
+            ]
+        let expected =
+            [
+                Point (1.0)
+            ]
+        Assert.AreEqual(expected, segmentIntersectionList s1 segs)
+
+    [<TestMethod>]
+    member this.TestSegmentItersectionListOverlap1 () =
+        let s1 = seg (0,0) (4,4)
+        let segs =
+            [
+                seg (1,1) (2,2)
+                seg (1,2) (2,3)
+                seg (0,1) (1,2)
+            ]
+        let expected =
+            [
+                Overlap (0.25, 0.5)
+            ]
+        Assert.AreEqual(expected, segmentIntersectionList s1 segs)
+
+    [<TestMethod>]
+    member this.TestSegmentItersectionListOverlap2 () =
+        let s1 = seg (1,1) (-1,-1)
+        let segs =
+            [
+                seg (0,0) (2,2)
+                seg (1,2) (2,3)
+                seg (0,1) (1,2)
+            ]
+        let expected =
+            [
+                Overlap (0.0, 0.5)
+            ]
+        Assert.AreEqual(expected, segmentIntersectionList s1 segs)
+
+    [<TestMethod>]
+    member this.TestSegmentItersectionListOverlap3 () =
+        let s1 = seg (0,0) (4,0)
+        let segs =
+            [
+                seg (1,1) (1,0)
+                seg (1,0) (0,0)
+                seg (0,1) (0,0)
+            ]
+        let expected =
+            [
+                Point (0.25)
+                Overlap (0.0, 0.25)
+                Point (0.0)
+            ]
+        Assert.AreEqual(expected, segmentIntersectionList s1 segs)
+
+[<TestClass>]
+type GeometryDecomposeTestClass () =
+
+    // Helpers
+    let seg (x1,y1) (x2,y2) = (Coord (x1,y1), Coord (x2,y2))
+
+    [<TestMethod>]
+    member this.TestSegmentDecompose1 () =
+        let s1 = seg (0,0) (4,0)
+        let segs =
+            [
+                seg (1,1) (1,0)
+                seg (1,1) (0,1)
+                seg (1,0) (0,0)
+                seg (0,1) (0,0)
+            ]
+        let expected =
+            [
+                Overlap (0.0, 0.25)
+            ]
+        Assert.AreEqual(expected, segmentIntersectionList s1 segs)
