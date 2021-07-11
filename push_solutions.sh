80bef9bad85cd491c1460c5fb5990b3db4f151a0
Johan Rosenkilde <jsrn@jsrn.dk>
2021-07-12 00:58:08 +0200

    Safer push_solutions

--- push_solutions.sh
+++ push_solutions.sh
@@ -8,11 +8,13 @@ for i in {1..132}; do
         SMALLEST=$(cd problems/$i-solutions/; ls * | sort -n | head -1)
         if [ ! -f problems/$i-solutions/$SMALLEST.submitted ]; then
             echo "Submitting $i-solutions/$SMALLEST"
-            curl -H "Authorization: Bearer $APIKEY" --data @problems/$i-solutions/$SMALLEST https://poses.live/api/problems/$i/solutions
-            echo
-            touch problems/$i-solutions/$SMALLEST.submitted
-            git add problems/$i-solutions/$SMALLEST
-            git add problems/$i-solutions/$SMALLEST.submitted
+            curl -sS -H "Authorization: Bearer $APIKEY" --data @problems/$i-solutions/$SMALLEST https://poses.live/api/problems/$i/solutions | tee /dev/stderr | jq -e '.id'
+            if [ $? = 0 ]; then
+                echo
+                touch problems/$i-solutions/$SMALLEST.submitted
+                git add problems/$i-solutions/$SMALLEST
+                git add problems/$i-solutions/$SMALLEST.submitted
+            fi
         fi
     fi
 done
