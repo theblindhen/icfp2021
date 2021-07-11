d14ab8b15ae7b46a20cb77ad3b0cf932fbff8eb5
Johan Rosenkilde <jsrn@jsrn.dk>
2021-07-12 01:38:46 +0200

    solutions and bugfix in push_solutions

--- push_solutions.sh
+++ push_solutions.sh
@@ -5,15 +5,17 @@ if [ -z $APIKEY ]; then
 fi
 for i in {1..132}; do
     if [ -d problems/$i-solutions ]; then
-        SMALLEST=$(cd problems/$i-solutions/; ls * | sort -n | head -1)
-        if [ ! -f problems/$i-solutions/$SMALLEST.submitted ]; then
-            echo "Submitting $i-solutions/$SMALLEST"
-            curl -sS -H "Authorization: Bearer $APIKEY" --data @problems/$i-solutions/$SMALLEST https://poses.live/api/problems/$i/solutions | tee /dev/stderr | jq -e '.id'
-            if [ $? = 0 ]; then
-                echo
-                touch problems/$i-solutions/$SMALLEST.submitted
-                git add problems/$i-solutions/$SMALLEST
-                git add problems/$i-solutions/$SMALLEST.submitted
+        if [ -z $(ls problems/$i-solutions) ]; then
+            SMALLEST=$(cd problems/$i-solutions/; ls * | sort -n | head -1)
+            if [ ! -f problems/$i-solutions/$SMALLEST.submitted ]; then
+                echo "Submitting $i-solutions/$SMALLEST"
+                curl -sS -H "Authorization: Bearer $APIKEY" --data @problems/$i-solutions/$SMALLEST https://poses.live/api/problems/$i/solutions | tee /dev/stderr | jq -e '.id'
+                if [ $? = 0 ]; then
+                    echo
+                    touch problems/$i-solutions/$SMALLEST.submitted
+                    git add problems/$i-solutions/$SMALLEST
+                    git add problems/$i-solutions/$SMALLEST.submitted
+                fi
             fi
         fi
     fi
